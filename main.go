17f8eaacc09ad553f1ef8ea1588cd4ea6ff6ce23
cramanan <cramananjaonapro@gmail.com>
2026-09-11 20:54:04 +0200

    feat: added API fetching example

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	_ "embed"
+	"encoding/json/v2"
 	"govite/vite"
 	"log"
 	"net/http"
+	"strings"
 
 	inertia "github.com/romsar/gonertia/v3"
 )
@@ -22,16 +24,34 @@ func main() {
 		log.Fatal(err)
 	}
 
+	http.HandleFunc("/api/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.MarshalWrite(w, map[string]string{"hello": "world"})
+	})
+
 	// Now create your HTTP server.
 	// Gonertia works well with standard http server library,
 	// but you are free to use some external routers like Gorilla Mux or Chi.
 	if dist := vite.Dist(); dist != nil {
-		http.Handle("/build/", http.StripPrefix("/build/", http.FileServerFS(dist)))
+		fs := http.FileServerFS(dist)
+		http.Handle("/build/", http.StripPrefix("/build/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "" || strings.HasSuffix(r.URL.Path, "/") {
+				http.NotFound(w, r)
+				return
+			}
+			fs.ServeHTTP(w, r)
+		})))
 	}
+
 	http.Handle("/", vi.Middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
 		if err := vi.Render(w, r, "index", inertia.Props{"key": "value"}); err != nil {
 			log.Println(err)
 		}
+
 	})))
 
 	http.ListenAndServe(":8080", nil)
--- resources/js/pages/index.svelte
+++ resources/js/pages/index.svelte
@@ -2,10 +2,25 @@
   // import Layout from "@/layouts/layout.svelte";
   // export const layout = Layout;
   export { default as layout } from "@/layouts/layout.svelte";
-
-  let count = $state(0);
 </script>
 
-Hello World
+<script lang="ts">
+  let count = $state(0);
+  let status = $state();
 
-<button onclick={() => count++}>{count}</button>
+  $effect(
+    () =>
+      void fetch("/api/status")
+        .then((response) => response.json())
+        .then((value) => (status = value)),
+  );
+</script>
+
+<h1>Hello World</h1>
+
+<div>
+  <button onclick={() => count++}>{count}</button>
+  <code style="white-space: pre-wrap;">
+    {JSON.stringify(status, null, "\t")}
+  </code>
+</div>
